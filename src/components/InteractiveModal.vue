4d8ddeabee87c875320eb8474cd433ad21b616ea
Augustine Nwafor <austineforall@gmail.com>
2021-02-10 16:33:28 +0100

    'AMPInertactions'

--- src/components/InteractiveModal.vue
+++ src/components/InteractiveModal.vue
@@ -1,47 +1,164 @@
 <template>
 <div id="interactiveModal" class="interactiveModal animate__animated animate__fadeInUp">
-  <div class="overlay" @click="removeInteractiveMdal"></div>
+  <div class="overlay" @click="removeInteractiveModal"></div>
   <div class="mainDiv">
     <template>
-      <label>
-        <span>Title</span>
-        <input type="text">
-      </label>
       <label style="flex: 1 1 100%">
         <span>Question</span>
-        <input type="text">
+        <input type="text" v-model="question">
       </label>
       <label>
         <span>Option 1</span>
-        <input type="text">
+        <span class="inputBox">
+          <input type="text" v-model="option1">
+          <v-menu
+            top
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color=""
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span v-if="!option1Confetti">Emoji</span>
+                <span style="font-size: 2em" v-if="option1Confetti">{{option1Confetti}}</span>
+              </v-btn>
+            </template>
+
+            <picker title="" @select="showOption1Confetti" :data="emojiIndex" set="google" />
+          </v-menu>
+        </span>
       </label>
       <label>
         <span>Option 2</span>
-        <input type="text">
+        <span class="inputBox">
+          <input type="text" v-model="option2">
+          <v-menu
+            top
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color=""
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span v-if="!option2Confetti">Emoji</span>
+                <span style="font-size: 2em" v-if="option2Confetti">{{option2Confetti}}</span>
+              </v-btn>
+            </template>
+
+            <picker title="" @select="showOption2Confetti" :data="emojiIndex" set="google" />
+          </v-menu>
+        </span>
       </label>
-      <label>
-        <span>Option 3</span>
-        <input type="text">
+      <label v-if="isBinaryPool === false">
+        <span>Option 3 <small>(optional)</small></span>
+        <span class="inputBox">
+          <input type="text" v-model="option3">
+          <v-menu
+            top
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color=""
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span v-if="!option3Confetti">Emoji</span>
+                <span style="font-size: 2em" v-if="option3Confetti">{{option3Confetti}}</span>
+              </v-btn>
+            </template>
+
+            <picker title="" @select="showOption3Confetti" :data="emojiIndex" set="google" />
+          </v-menu>
+        </span>
       </label>
-      <label>
-        <span>Option 4</span>
-        <input type="text">
+      <label v-if="isBinaryPool === false">
+        <span>Option 4 <small>(optional)</small></span>
+        <span class="inputBox">
+          <input type="text" v-model="option4">
+          <v-menu
+            top
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color=""
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span v-if="!option4Confetti">Emoji</span>
+                <span style="font-size: 2em" v-if="option4Confetti">{{option4Confetti}}</span>
+              </v-btn>
+            </template>
+
+            <picker title="" @select="showOption4Confetti" :data="emojiIndex" set="google" />
+          </v-menu>
+        </span>
       </label>
 
-      <div>
-        <button>Save Quiz</button>
-      </div>
+      <v-btn block @click="saveQuiz" color="#519EF4">
+        Save
+      </v-btn>
     </template>
   </div>
 </div>
 </template>
 
 <script>
+// import axios from "axios";
+import data from 'emoji-mart-vue-fast/data/all.json'
+import { Picker, EmojiIndex } from 'emoji-mart-vue-fast'
+let emojiIndex = new EmojiIndex(data)
+
 export default {
 name: "InteractiveModal",
 
+  data() {
+    return {
+      baseUrl: process.env.VUE_APP_baseUrl,
+      authToken: `Bearer ${localStorage.getItem('authToken')}`,
+      theme: 'light',
+      emojiIndex: emojiIndex,
+      question: '',
+      option1: '',
+      option1Confetti: '',
+      option2: '',
+      option2Confetti: '',
+      option3: '',
+      option3Confetti: '',
+      option4: '',
+      option4Confetti: '',
+    }
+  },
+
+  props: {
+    isBinaryPool: undefined,
+    interactionType: undefined
+  },
+
   methods: {
-    removeInteractiveMdal() {
+    showOption1Confetti(emoji) {
+      this.option1Confetti = emoji.native;
+    },
+    showOption2Confetti(emoji) {
+      this.option2Confetti = emoji.native;
+    },
+    showOption3Confetti(emoji) {
+      this.option3Confetti = emoji.native;
+    },
+    showOption4Confetti(emoji) {
+      this.option4Confetti = emoji.native;
+    },
+
+    removeInteractiveModal() {
       const div = document.getElementById('interactiveModal')
       div.classList.remove('animate__fadeInUp')
       div.classList.add('animate__fadeOutDown')
@@ -52,13 +169,97 @@ name: "InteractiveModal",
     },
 
     saveQuiz() {
+      console.log(this.interactionType)
+      switch (this.interactionType) {
+        case 'binaryPoll': this.createBinaryPoll(); break;
+        case 'poll': this.createPoll(); break;
+        case 'quiz': this.createQuiz();
+      }
+    },
 
-    }
+    createPoll() {
+      const page = document.getElementById('page')
+      const interactivePoll =  document.createElement('amp-story-interactive-poll')
+
+      interactivePoll.setAttribute('endpoint', 'https://backend.com/v1/interactives');
+      interactivePoll.setAttribute('theme', this.theme);
+      interactivePoll.setAttribute('prompt-text', this.question);
+
+      interactivePoll.setAttribute('option-1-text', this.option1);
+      interactivePoll.setAttribute('option-1-confetti', this.option1Confetti);
+      interactivePoll.setAttribute('option-2-text', this.option2);
+      interactivePoll.setAttribute('option-2-confetti', this.option2Confetti);
+      interactivePoll.setAttribute('option-3-text', this.option3);
+      interactivePoll.setAttribute('option-3-confetti', this.option3Confetti);
+      interactivePoll.setAttribute('option-4-text', this.option4);
+      interactivePoll.setAttribute('option-4-confetti', this.option4Confetti);
+
+      interactivePoll.dataset.data = `{theme: ${this.theme}, prompt-text: ${this.question}, option-1-text: ${this.option1}, option-1-confetti: ${this.option1Confetti}, option-2-text: ${this.option2}, option-2-confetti: ${this.option2Confetti}, option-3-text: ${this.option3}, option-3-confetti: ${this.option3Confetti}, option-4-text: ${this.option4}, option-4-confetti: ${this.option4Confetti}}`
+      interactivePoll.style.width = '250px';
+      interactivePoll.style.height = '300px';
+      interactivePoll.style.display = 'block';
+      interactivePoll.style.overflow = 'hidden';
+      interactivePoll.style.position = 'absolute';
+      interactivePoll.classList.add('pollInteraction', 'resize-drag')
+      interactivePoll.id = Date.now().toString()
+      interactivePoll.innerHTML = `<div class="question"> ${this.question} </div><div class="options" style="background: ${this.theme === "dark" ? '#202124' : ''}; color: ${this.theme === "dark" ? '#ffffff' : ''}"><div>${this.option1}</div><div>${this.option2}</div><div>${this.option3}</div><div>${this.option4}</div></div>`
+
+      page.appendChild(interactivePoll)
+      this.$parent.$parent.updatePageStructure();
+      // axios.put(`${this.baseUrl}quiz/create`, null, {headers: {Authorization: this.authToken}})
+      //     .then(res => {
+      //       console.log('Saved and Updated', res)
+      //       sessionStorage.setItem('storiURL', res.data.data.amp_file)
+      //     })
+    },
+
+    createQuiz() {
+      const page = document.getElementById('page')
+      const interactiveQuiz =  document.createElement('amp-story-interactive-quiz')
+
+      interactiveQuiz.setAttribute('endpoint', 'https://backend.com/v1/interactives');
+      interactiveQuiz.setAttribute('theme', this.theme);
+      interactiveQuiz.setAttribute('prompt-text', this.question);
+
+      interactiveQuiz.setAttribute('option-1-text', this.option1);
+      interactiveQuiz.setAttribute('option-1-confetti', this.option1Confetti);
+      interactiveQuiz.setAttribute('option-2-text', this.option2);
+      interactiveQuiz.setAttribute('option-2-confetti', this.option2Confetti);
+      interactiveQuiz.setAttribute('option-3-text', this.option3);
+      interactiveQuiz.setAttribute('option-3-confetti', this.option3Confetti);
+      interactiveQuiz.setAttribute('option-4-text', this.option4);
+      interactiveQuiz.setAttribute('option-4-confetti', this.option4Confetti);
+
+      interactiveQuiz.dataset.data = `{theme: ${this.theme}, prompt-text: ${this.question}, option-1-text: ${this.option1}, option-1-confetti: ${this.option1Confetti}, option-2-text: ${this.option2}, option-2-confetti: ${this.option2Confetti}, option-3-text: ${this.option3}, option-3-confetti: ${this.option3Confetti}, option-4-text: ${this.option4}, option-4-confetti: ${this.option4Confetti}}`
+      interactiveQuiz.style.width = '250px';
+      interactiveQuiz.style.height = '300px';
+      interactiveQuiz.style.display = 'block';
+      interactiveQuiz.style.overflow = 'hidden';
+      interactiveQuiz.style.position = 'absolute';
+      interactiveQuiz.classList.add('pollInteraction', 'resize-drag')
+      interactiveQuiz.id = Date.now().toString()
+      interactiveQuiz.innerHTML = `<div class="question"> ${this.question} </div><div class="options" style="background: ${this.theme === "dark" ? '#202124' : ''}; color: ${this.theme === "dark" ? '#ffffff' : ''}"><div>${this.option1}</div><div>${this.option2}</div><div>${this.option3}</div><div>${this.option4}</div></div>`
+
+      page.appendChild(interactiveQuiz)
+      this.$parent.$parent.updatePageStructure();
+      // axios.put(`${this.baseUrl}quiz/create`, null, {headers: {Authorization: this.authToken}})
+      //     .then(res => {
+      //       console.log('Saved and Updated', res)
+      //       sessionStorage.setItem('storiURL', res.data.data.amp_file)
+      //     })
+    },
+
+  },
+
+  components: {
+    'Picker': Picker
   }
 }
 </script>
 
 <style lang="scss">
+@import '~emoji-mart-vue-fast/css/emoji-mart.css';
+
 .interactiveModal {
   display: flex;
   justify-content: center;
@@ -80,12 +281,13 @@ name: "InteractiveModal",
   .mainDiv {
     width: 65%;
     max-width: 1050px;
-    height: 80%;
+    //height: 80%;
     background: #1B1C1E;
     border-radius: 10px;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+    align-items: center;
     padding: 100px;
     color: #a5d1e7;
     overflow-y: auto;
@@ -111,6 +313,30 @@ name: "InteractiveModal",
 
         &:focus {
           box-shadow: 0 0 5px #a5d1e7;
+        }
+      }
+      .inputBox {
+        display: flex;
+
+        button {
+          height: 50px;
+          padding: 5px;
+          margin: auto 5px;
+        }
+
+        input, textarea {
+          display: block;width: 100%;
+          height: 50px;
+          border: solid 1px #a5d1e7;
+          border-radius: 5px;
+          outline: none;
+          padding: 5px;
+          color: #a5d1e7;
+          font-size: 1.2em;
+
+          &:focus {
+            box-shadow: 0 0 5px #a5d1e7;
+          }
         }
       }
       .uploadImage {
--- src/components/InteractivePanel.vue
+++ src/components/InteractivePanel.vue
@@ -1,18 +1,7 @@
 <template>
   <div>
       <!--    <h4>Binary Poll</h4>-->
-    <amp-story-interactive-binary-poll
-        draggable="true"
-        @mousedown="dragMedia($event)"
-        class="binaryInteraction"
-        endpoint="https://backend.com/v1/interactives"
-        prompt-text="Like Pizza?"
-        option-1-text="Yes"
-        option-1-confetti="🍕"
-        option-2-text="No"
-        option-2-confetti="🤢"
-        v-on:click="isModalActive = true"
-    >
+    <div class="binaryInteraction" v-on:click="isBinaryPool = true; interactionType = 'binaryPoll'; isModalActive = true">
       <div class="question">
         Like Pizza?
       </div>
@@ -20,30 +9,63 @@
         <div>Yes</div>
         <div>No</div>
       </div>
-    </amp-story-interactive-binary-poll>
+    </div>
 
 <!--    <h4>Poll</h4>-->
-<!--    <amp-story-interactive-poll-->
-<!--        draggable="true"-->
-<!--        @mousedown="dragMedia($event)"-->
-<!--        class="pollInteraction"-->
+
+
 <!--        endpoint="https://backend.com/v1/interactives"-->
 <!--        prompt-text="Pick a season"-->
 <!--        option-1-text="Spring" option-1-confetti="🌼"-->
 <!--        option-2-text="Summer" option-2-confetti="☀"-->
 <!--        option-3-text="Fall" option-3-confetti="🍁"-->
 <!--        option-4-text="Winter" option-4-confetti="☃">-->
-<!--    >-->
-<!--      <div class="question">-->
-<!--        Like Pizza?-->
-<!--      </div>-->
-<!--      <div class="options">-->
-<!--        <div>Yes</div>-->
-<!--        <div>No</div>-->
-<!--      </div>-->
-<!--    </amp-story-interactive-poll>-->
+    <div class="pollInteraction"  v-on:click="isBinaryPool = false; interactionType = 'poll'; isModalActive = true">
+      <div class="question">
+        Pick a season
+      </div>
+      <div class="options">
+        <div>Spring</div>
+        <div>Summer</div>
+        <div>Fall</div>
+        <div>Winter</div>
+      </div>
+    </div>
 
-    <InteractiveModal v-if="isModalActive === true"></InteractiveModal>
+
+    <div class="pollInteraction"  v-on:click="isBinaryPool = false; interactionType = 'quiz'; isModalActive = true">
+      <div class="question">
+        Who was the artist that created the famous painting "The Last Supper"?
+      </div>
+      <div class="options">
+        <div>
+          <div>
+            <span>A</span>
+          </div>
+          Michelangelo
+        </div>
+        <div>
+          <div>
+            <span>B</span>
+          </div>
+          Leonardo da Vinci
+        </div>
+        <div>
+          <div>
+            <span>C</span>
+          </div>
+          Rahael
+        </div>
+        <div>
+          <div>
+            <span>D</span>
+          </div>
+          Sandro Boticelli
+        </div>
+      </div>
+    </div>
+
+    <InteractiveModal v-if="isModalActive === true" :is-binary-pool="isBinaryPool" :interaction-type="interactionType"></InteractiveModal>
   </div>
 </template>
 
@@ -53,7 +75,9 @@ export default {
 name: "InteractivePanel",
   data() {
     return {
-      isModalActive: false
+      isModalActive: false,
+      isBinaryPool: undefined,
+      interactionType: undefined,
     }
   },
 
@@ -122,4 +146,74 @@ name: "InteractivePanel",
     }
   }
 }
+
+.pollInteraction {
+  width: 250px;
+  margin: 20px 35px;
+  border-radius: 40px 40px 20px 20px;
+  background: #0259ED;
+  height: max-content;
+
+  &:hover {
+    &:before {
+      content: '';
+      position: absolute;
+      width: 250px;
+      height: 150px;
+      z-index: 1;
+    }
+  }
+  .question {
+    position: relative !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.6em;
+    font-weight: 600;
+    min-height: 65px;
+    padding: 15px 10px;
+    height: max-content;
+  }
+
+  .options {
+    position: relative !important;
+    width: 100%;
+    height: max-content;
+    border-radius: 20px;
+    background: white;
+    //display: flex;
+    color: black;
+    padding: 10px;
+
+    > div {
+      position: relative !important;
+      box-shadow: 0 0 2px #7c7c7c;
+      margin: 10px auto;
+      border-radius: 10px;
+      box-sizing: border-box;
+      font-weight: 500;
+      font-size: 1em;
+      color: inherit;
+      padding: 10px;
+      div {
+        span {
+          //position: absolute;
+          border-radius: 100%;
+          width: 20px;
+          height: 20px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 1px solid #0a81be;
+          padding: 12px;
+        }
+      }
+      &:first-child {
+        border-right: 1px solid #EAEAEA;
+      }
+    }
+  }
+}
+
+
 </style>
